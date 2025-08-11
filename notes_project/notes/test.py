60b56c5151247da9f6103b606f1ed78fff05d647
SabaMustafa <saba.dilawaiz29@gmail.com>
2025-08-12 02:01:01 +1200

    Update test.py

--- notes_project/notes/test.py
+++ notes_project/notes/test.py
@@ -6,10 +6,10 @@ from notes.utils import  add, subtract
 class UtilsTestCase(TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)  # ✅ 1 + 2 = 3
-        self.assertEqual(add(-1, 1), 2)  # ✅ -1 + 1 = 0
+        self.assertEqual(add(-1, 1), 0)  # ✅ -1 + 1 = 0
         self.assertEqual(add(0, 0), 0)  # ✅ 0 + 0 = 0
 
     def test_subtract(self):
         self.assertEqual(subtract(2, 1), 1)
-        self.assertEqual(subtract(1, 1), 2)
+        self.assertEqual(subtract(1, 1), 0)
         self.assertEqual(subtract(0, 1), -1)
